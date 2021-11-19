f60cc87b4649e22e26673f12ac13ae8d81d4f36d
atefganm <atef_anim_1974@yahoo.com>
2021-11-19 22:13:50 +0200

    update-15

--- lib/python/Components/AVSwitch.py
+++ lib/python/Components/AVSwitch.py
@@ -363,11 +363,14 @@ class AVSwitch:
                         wss = "auto(4:3_off)"
                 else:
                         wss = "auto"
+            try:
                 if os.path.exists("/proc/stb/denc/0/wss"):
                         print "[AVSwitch] setting wss: %s" % wss
                         f = open("/proc/stb/denc/0/wss", "w")
                         f.write(wss)
                         f.close()
+            except:
+                pass
 
         def setPolicy43(self, cfgelement):
                 print "[AVSwitch] setting policy: %s" % cfgelement.value
